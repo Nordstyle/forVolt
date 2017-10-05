c77310826ee6a6e2e14a3c1a66ad314b5627f1b6
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-10-06 00:27:55 +0700

    add navigation menu

---
+++ src/Menu.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+
+export default () => (
+  <div>
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/">Invoice App</Link>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <LinkContainer to="/customers">
+          <NavItem eventKey={2}>Customers</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/products">
+          <NavItem eventKey={1}>Products</NavItem>
+        </LinkContainer>
+      </Nav>
+    </Navbar>
+  </div>
+);
