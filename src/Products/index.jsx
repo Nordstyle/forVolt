4cb708d2554d6990337cdd19042e6c583c7d0c35
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-10-06 00:29:48 +0700

    products page

---
+++ src/Products/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    return (
+      <div>
+        Products Page!
+      </div>
+    )
+  }
+};
